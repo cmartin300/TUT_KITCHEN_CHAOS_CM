09498da8139ddc8a0a57f9e21333c2c84ba347e8
Chris <martin.christophercharles@gmail.com>
2024-04-14 20:40:35 -0700

    collisions and interactions
    added collisions with raycasts
    added interactable counters with raycasts and layers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,31 +2,99 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float rotationSpeed = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastMoveDir;
 
     private void Update()
     {
-        Vector2 inputVector = inputManager.GetMovementVectorNormalized();
-
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        isWalking = moveDir != Vector3.zero;
-
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-
+        HandleMovement();
+        HandleInteractions();
     }
 
     public bool IsWalking()
     {
         return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = inputManager.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastMoveDir = moveDir;
+        }
+
+        float interactDistance = 1.2f;
+        if (Physics.Raycast(transform.position, lastMoveDir, out RaycastHit hitInfo, interactDistance, countersLayerMask))
+        {
+            if (hitInfo.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = inputManager.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        isWalking = moveDir != Vector3.zero;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerHeight = 2f;
+        float playerRadius = 0.65f;
+        bool canMove = !Physics.CapsuleCast(
+            transform.position,
+            transform.position + transform.up * playerHeight,
+            playerRadius,
+            moveDir,
+            moveDistance
+        );
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + transform.up * playerHeight,
+                playerRadius,
+                moveDirX,
+                moveDistance
+            );
+
+            if (canMove) { moveDir = moveDirX.normalized; }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + transform.up * playerHeight,
+                    playerRadius,
+                    moveDirZ,
+                    moveDistance
+                );
+
+                if (canMove) { moveDir = moveDirZ.normalized; }
+            }
+        }
+
+        if (canMove) { transform.position += moveDir * moveDistance; }
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
 }
--- README.md
+++ README.md
@@ -12,8 +12,11 @@ https://www.youtube.com/watch?v=AmGSEH7QcDg&t=4149s
   - Setup player with legacy input
   - Setup player movement and rotation
 
-- Progress 04/14/2024: 2:04:00;
+- Progress 04/14/2024: 2:38:20;
   - Setup animations for idle and walking
   - Setup Cinemachine camera
   - Refactored to new input system
   - setup for WASD and Controller input
+  - Setup raycasts for collision detection
+  - Created clear counter prefab
+  - Setup interactable handling with raycasts & layers
