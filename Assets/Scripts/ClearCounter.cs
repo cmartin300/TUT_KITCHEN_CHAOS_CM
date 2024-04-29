c035d82ce664d708fabdcbf1aa53e065be4c2a5b
Chris <martin.christophercharles@gmail.com>
2024-04-29 13:20:44 -0700

    swapping counters
    added logic and visuals for swapping kitchen objects between counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,18 +7,58 @@ public class ClearCounter : MonoBehaviour
 
     [SerializeField] private KitchenObjectSO spawnObject;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
 
     private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
         if (kitchenObject == null)
         {
             Transform objectTransform = Instantiate(spawnObject.prefab, spawnPoint);
-            objectTransform.localPosition = Vector3.zero;
-
-            kitchenObject = objectTransform.GetComponent<KitchenObject>();
+            objectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetKitchenObjectSO().objectName + " on " + kitchenObject.GetClearCounter());
         }
 
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return spawnPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
