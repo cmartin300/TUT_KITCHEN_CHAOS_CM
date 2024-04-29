8100727e8681d4c9a223e20f7bb35ae563b9d102
Chris <martin.christophercharles@gmail.com>
2024-04-29 08:44:46 -0700

    spawn check
    added check to clear counter to only allow spawning one object.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,12 +8,17 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private KitchenObjectSO spawnObject;
     [SerializeField] private Transform spawnPoint;
 
+    private KitchenObject kitchenObject;
+
     public void Interact()
     {
-        Debug.Log($"Interact!");
-        Transform objectTransform = Instantiate(spawnObject.prefab, spawnPoint);
-        objectTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null)
+        {
+            Transform objectTransform = Instantiate(spawnObject.prefab, spawnPoint);
+            objectTransform.localPosition = Vector3.zero;
 
-        Debug.Log(objectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+            kitchenObject = objectTransform.GetComponent<KitchenObject>();
+        }
+
     }
 }
