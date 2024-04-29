c338a1094babadd9707258c0a2f089c305bb294a
Chris <martin.christophercharles@gmail.com>
2024-04-28 23:34:35 -0700

    Scriptable Objects
    Created scriptable objects for spawning ingredients on clear counters.
    Created for tomato and cheese block.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private KitchenObjectSO spawnObject;
+    [SerializeField] private Transform spawnPoint;
+
     public void Interact()
     {
-        Debug.Log($"Interacted with {this.gameObject.name}");
+        Debug.Log($"Interact!");
+        Transform objectTransform = Instantiate(spawnObject.prefab, spawnPoint);
+        objectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(objectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
